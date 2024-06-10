469a96d94d876a18738781ad4a1d662446d1f23d
Sadaf Mahar <169028965+SadafMahar@users.noreply.github.com>
2024-06-10 09:40:32 +0500

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//let firstName = "ali";
+//number + number addition
+//num + string concatenation 
+//string + number concatenation
+//let name = "ali";
+//let name2 = "feroze";
+//let rendom number1 = match.rendom1;
+//console.log(rendomNumber1)
+//let x = 33;
+//console.log(x);
+//console.log(typeof(x));
+//let name = false;
+//console.log(name);
+//let name = null;
+//console.log(name);
+//Boolean
+//let x = 33;
+//let x = true;
+//console.log (typeof y);
+//console.log (x + y);
+//let x = 33;
+ //let y = true;
+ //console.log(x + 120 + y);
